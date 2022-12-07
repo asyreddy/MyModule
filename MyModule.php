2d861aa3a40183ca7197d519fe81c1375eaa0f04
asyreddy <85878679+asyreddy@users.noreply.github.com>
2022-12-07 12:00:46 +0530

    Add files via upload

---
+++ MyModule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Joueclub\MyModule\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class MyModule extends Template
+{
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+//    public function getMymodule()
+//    {
+//        return 'Module Created Successfully';
+//    }
+}
